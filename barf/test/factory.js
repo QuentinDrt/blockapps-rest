0425e22f8eecd5ab7533959524b084ac92541063
Tanuj Soni <tanuj@blockapps.net>
2019-03-08 19:36:00 +0530

    [SER-15_chainEndPoints] Fix: minor bugs

--- barf/test/factory.js
+++ barf/test/factory.js
@@ -2,8 +2,8 @@ import rest from '../rest_7'
 import util from '../util'
 import fsUtil from '../fsUtil'
 
-const ENODE = 'enode://6d8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0@171.16.0.4:30303?discport=30303';
-const BALANCE = 100000000000000000000;
+const enode = 'enode://6d8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0@171.16.0.4:30303?discport=30303';
+const balance = 100000000000000000000;
 
 /*
   users
@@ -85,11 +85,11 @@ function createCallMethodArgsArr(contract, args, method = 'multiply', count = 2)
 }
 
 const createChainArgs = (members) => {
-  const memberList = members.map((address) => { return ({ address: address, enode: ENODE }) });
-  const balanceList = members.map((address) => { return ({ address: address, balance: BALANCE }) });
+  const memberList = members.map((address) => { return ({ address: address, enode }) });
+  const balanceList = members.map((address) => { return ({ address: address, balance }) });
 
   const chain = {
-    label: `test airline ${util.uid()}`,
+    label: `airline-${util.uid()}`,
     src: 'contract Governance { }',
     args: {},
     members: memberList,
