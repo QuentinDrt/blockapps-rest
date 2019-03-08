8de16468c550efd55506f6996e300ea24ae70434
Tanuj Soni <tanuj@blockapps.net>
2019-03-08 19:45:24 +0530

    [SER-15_chainEndPoints] genralized create of chain

--- barf/test/factory.js
+++ barf/test/factory.js
@@ -1,8 +1,12 @@
 import rest from '../rest_7'
 import util from '../util'
 import fsUtil from '../fsUtil'
+import ip from 'ip'
 
-const enode = 'enode://6d8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0@171.16.0.4:30303?discport=30303';
+const publicKey = '6d8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0';
+const port = 30303;
+const localIp = ip.address();
+const enode = `enode://${publicKey}@${localIp}:${port}`
 const balance = 100000000000000000000;
 
 /*
